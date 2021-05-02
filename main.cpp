a6ccb56cfd6361177dd1a7e48678aa9cc109dd58
Carlos Menezes <carlos-menezes212@hotmail.com>
2021-05-02 19:55:04 +0100

    Show usage

--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 #include <time.h>
 
 #include "utils/runtime.h"
+#include "utils/io.h"
 
 int main(int argc, char **argv) {
     srand(time(nullptr));
 
-    if (argc == 1) return runtime::asNew();
-    else if (argc == 2) return runtime::fromFile(argv[1]);
+    if (argc == 1) runtime::asNew();
+    else if (argc == 2) runtime::fromFile(argv[1]);
+    else io::output::info("USAGE: super_eda [file.txt]");
 }
